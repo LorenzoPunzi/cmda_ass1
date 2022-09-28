1220cff6d62fd484082ce25d21c9acf490d4fb94
Lorenzo Punzi <lorenzo@Air-di-Lorenzo-016.homenet.telecomitalia.it>
2022-09-29 01:52:01 +0200

    Modified bookstat.py removing spaces in favor of tabs so to make it runnable. Now works

--- bookstat.py
+++ bookstat.py
@@ -9,8 +9,9 @@ import argparse
 def process(file_path):
 
 	print(f'Opening input file {file_path}...') #The f is used to use the {} notation->very convenient
-    with open(file_path,'r') as input_file: #with ensures the file is automatically closed at the end
-	    text=input_file.read() #reads till end of file
+	with open(file_path,'r') as input_file:
+		text=input_file.read() #reads till end of file
+
 	print(text)
 	print('Done.')
 
@@ -18,6 +19,6 @@ def process(file_path):
 if __name__=='__main__': #checks wether this module is called as main script from command line
 
 	parser = argparse.ArgumentParser(description='Print some book statistics')    #gives options to the program when called
-    parser.add_argument('infile', type=str, help='path to the input file')       #gives an option of string and sets up what --help says for that option
-    args = parser.parse_args()        #always do it: it's what you get from command line as option
-    process(args.infile)              #gives the function the object given by command line as option infile
+	parser.add_argument('infile', type=str, help='path to the input file')       #gives an option of string and sets up what --help says for that option
+	args = parser.parse_args()        #always do it: it's what you get from command line as option
+	process(args.infile)              #gives the function the object given by command line as option infile
